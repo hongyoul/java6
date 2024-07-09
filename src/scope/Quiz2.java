8ecca2ec1a207582add2b2e8c8d9bbe3bf3d30c3
Hongyoul <hongyoul03e@gmail.com>
2024-07-09 16:58:55 +0900

    2024.07.09 project6 오류로 인해 저장폴더 이름 변경

--- src/scope/Quiz2.java
+++ src/scope/Quiz2.java
@@ -1,23 +1,28 @@
 package scope;
 
 /* 
- * 모든 변수의 유효범위를 작성하세요 (인스턴스변수,클래스변수 / 전역변수,지역변수)
+ * 모든 변수의 유효범위를 작성하세요.
+ * 인스턴스변수,클래스변수 / 전역변수, 지역변수
+ * 구분에 따라 변수의 유효범위를 작성하세요.
  * */
+
 class Person {
+	
 	// 인스턴스 변수라서 객체 생성 후에 사용가능
 	// 전역변수라서 클래스 끝나기 전까지 사용가능
-	String personName;
-	int personAge; //
+	String personName; // 인스턴스변수 개별 속성
+	int personAge; // 인스턴스 변수 개별 속성
 
 	// 클래스 변수라서 객체없이도 사용 가능
 	// 전역변수라서 클래스 끝나기 전까지 사용가능
-	static char gender = 'F';
+	static char gender = 'F'; // static 사용 => 공통 속성 클래스 변수 *클래스 상단에 있다면(위치로 구분 가능) 전역변수로도 볼 수 있다
 
 	// 지역변수는 메소드 안에서만 사용가능
 	public void setPersonName(String name) {
-		this.personName = name;
+		this.personName = name; // 지역변수 : 잠깐 사용
 	}
 	
 }
+
 
 
--- src/scope/Quiz3.java
+++ src/scope/Quiz3.java
@@ -3,19 +3,21 @@ package scope;
 /* 
  * 모든 변수의 유효범위를 작성하세요 (블록스코프)
  * */
-public class Quiz3 {
 
-	public static void main(String[] args) {
+public class Quiz3 {
+	
+	// 블록에 따라 변수의 유효범위를 작성하세요(참조변수1, 기초변수3개 있음)
+	public static void main(String[] args) { // 범위를 찾을때 가장 가까운'{중가로}'를 찾으면 됨
 		
-		Person person = new Person(); //person는 메인함수가 끝나기 전까지 사용 가능
+		Person person = new Person(); // 기초변수: 'person'는 메인함수가 끝나기 전까지 사용 가능
 		
-		int i = 10; //i는 메인함수가 끝나기 전까지 사용 가능
+		int i = 10; // 기초변수: 'i'는 메인함수가 끝나기 전까지 사용 가능
 		
 		if(i > 1) {
-			String str = "안녕하세요"; //str은 if문 블록이 끝나기 전까지 사용 가능
+			String str = "안녕하세요"; // 기초변수: 'str'은 'if'문 블록이 끝나기 전까지 사용 가능
 		}
 		
-		for(int k=0; k<10; k++) { //k는 for문 블록이 끝나기 전까지 사용 가능
+		for(int k=0; k<10; k++) { // 참조변수: 'k'는 'for'문 블록이 끝나기 전까지 사용 가능
 			System.out.println();
 		}
 		
